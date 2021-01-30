17d22e94c16f352ae4b54f4575b273d619933801
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-30 14:38:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function igHover() {
+    var ig = document.getElementById("instagram");
+    ig.src = "assets/instagram-hover.png";
+}
+
+function igNormal() {
+    var ig = document.getElementById("instagram");
+    ig.src = "assets/instagram.png";
+}
+
+function ghHover() {
+    var gh = document.getElementById("github");
+    gh.src = "assets/github-hover.png";
+}
+
+function ghNormal() {
+    var gh = document.getElementById("github");
+    gh.src = "assets/github.png";
+}
